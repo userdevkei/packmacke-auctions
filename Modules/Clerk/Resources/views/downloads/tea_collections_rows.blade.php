116e2d181e086b7b023ff878e39dc195883298f6
userdevkei <kitilionesmus27@gmail.com>
2026-08-30 15:23:37 +0300

    restructured project

---
+++ Modules/Clerk/Resources/views/downloads/tea_collections_rows.blade.php
@@ -0,0 +1,19 @@
+@foreach($orders as $i => $order)
+    <tr>
+        <td>{{ $startIndex + $i + 1 }}</td>
+        <td>{{ $order->delivery_type == 1 ? 'DO Entry' : 'Direct Del' }}</td>
+        <td>{{ $order->invoice_number }}</td>
+        <td>{{ $order->garden_name }}</td>
+        <td>{{ $order->grade_name }}</td>
+        <td>{{ $order->order_number }}</td>
+        <td>{{ $order->lot_number }}</td>
+        <td>{{ $order->sale_number }}</td>
+        <td>{{ number_format($order->packet) }}</td>
+        <td>{{ $order->unit_weight == null ? number_format($order->weight, 2) : number_format($order->unit_weight, 2) }}</td>
+        <td>{{ number_format($order->sample_weight, 2) }}</td>
+        <td>{{ $order->difference == null ? '--' : $order->difference }}</td>
+        <td>{{ $order->warehouse_name }}</td>
+        <td>{{ \Carbon\Carbon::createFromTimestamp($order->date_received, config('app.timezone'))->format('Y-m-d') }}</td>
+        <td>{{ $order->status == null || $order->status == 1 ? 'Under Collection' : 'Collected' }}</td>
+    </tr>
+@endforeach
